81d5207e5c3dd0564f610bba98760a799abd27f8
Lizander35 <leon35.98@mail.ru>
2023-11-07 23:32:42 +0200

    add additional sass styling and arrowUp functionality

--- src/assets/components/Footer.jsx
+++ src/assets/components/Footer.jsx
@@ -1,20 +1,21 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { ComputerDesktopIcon } from "@heroicons/react/24/solid";
+import "../../styles/footer.scss";
 
 const Footer = () => {
   return (
-    <footer className="bg-white text-gray-800 py-4">
+    <footer>
       <div className="container mx-auto flex flex-col items-center md:flex-row md:justify-between">
         <Link
           to="/"
           className="font-bold text-2xl cursor-pointer flex items-center gap-1 mb-4 md:mb-0"
         >
           <ComputerDesktopIcon className="w-7 h-7 text-blue-600" />
-          <Link to="/">Leon</Link>
+          <Link to="/" className="text-blue-500">Leon</Link>
         </Link>
-        <div className="text-center md:text-right">
-          <div className="mb-2 md:mb-0">2023 copyright | portfolio</div>
+        <div className="copyright">
+          <div>2023 copyright | portfolio</div>
         </div>
       </div>
     </footer>
--- src/assets/components/Home.jsx
+++ src/assets/components/Home.jsx
@@ -1,14 +1,49 @@
+import { useState, useEffect } from "react";
 import React from "react";
 import Hero from "./Hero";
 import About from "./About";
 import Skills from "./Skills";
+import "../../styles/arrowUp.scss";
 
 const Home = () => {
+
+  const [showButton, setShowButton] = useState(false);
+
+  const amountScrolled = 200;
+
+  const handleScroll = () => {
+    if (window.scrollY > amountScrolled) {
+      setShowButton(true);
+    } else {
+      setShowButton(false);
+    }
+  };
+
+  const scrollToTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
+
   return (
     <div>
       <Hero />
       <About />
       <Skills />
+      <button
+        className={`back-to-top ${showButton ? "show" : ""}`}
+        onClick={scrollToTop}
+      />
     </div>
   );
 };
